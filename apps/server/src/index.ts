aa0b6d1fd9104a648160ccd9576fa4b42209b37a
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-06-22 21:05:24 +0300

    feat(server): add route for tRPC UI (Panel)

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import { toNodeHandler } from "@repo/auth/server";
+import { renderTrpcPanel } from 'trpc-ui';
 import { publicProcedure, router } from "./lib/trpc";
 import { auth } from "./lib/auth";
 import { createContext } from "./context";
@@ -8,12 +9,19 @@ import { createContext } from "./context";
 const app = express();
 
 const appRouter = router({
-    getHelloWorld: publicProcedure.query(() => {
-        return 'Hello, world!';
-    }),
+    getHelloWorld: publicProcedure
+        .query(() => {
+            return 'Hello, world!';
+        }),
 });
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use('/docs', (req, res) => {
+    res.send(renderTrpcPanel(appRouter, {
+        url: '/',
+    }))
+});
 
 app.use(createExpressMiddleware({
     router: appRouter,
